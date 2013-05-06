522f7353d37bec320d98dccc6048e59cbd9b22d5
Thomas Stassin <thomas.stassin@gmsproductions.com>
2013-05-06 20:57:23 +0200

    menu appears

--- game.py
+++ game.py
@@ -3,7 +3,8 @@
 import cocos
 import pyglet
 
-from data import OLD_COLOR, NORMAL_COLOR, TRANSITION, SCREEN_SIZE, SIDE_SCREEN, SLIDE_SPEED, TEST_MODE
+from data import OLD_COLOR, NORMAL_COLOR, TRANSITION, SCREEN_SIZE
+from data import SIDE_SCREEN, SLIDE_SPEED, TEST_MODE, TOP_SIDE, APPEARANCE_TIME
 
 class MultiScene(object):
 
@@ -21,15 +22,30 @@ class MultiScene(object):
         self.position = 0,0
         self.slide_scene = 0.
 
-        #add scheduler
+        self.inventory = Inventory()
+        self.in_menu = False
+
+
+
+        #add scheduler and inventory
         for scene in self.scene.values():
             scene.schedule(self.callback)
+            scene.add(self.inventory,z=10)
 
         cocos.director.director.window.push_handlers(self)
 
     def callback(self,dt):
         if self.slide_scene:
             self.slide()
+
+
+        self.inventory.appearance_time -= dt
+
+        if not self.in_menu:
+            if self.inventory.appearance_time <= 0:
+                self.inventory.appearance_time = APPEARANCE_TIME
+                self.inventory.appears(False)
+
 
     def get_active_scene(self):
         return self.scene[self.active]
@@ -53,7 +69,18 @@ class MultiScene(object):
 
     def on_mouse_motion(self,x,y,dx,dy):
 
-        SLIDE_SPEED
+        #inventory
+        if y <= TOP_SIDE:
+            self.in_menu = True
+            if self.inventory.position[1] < 0:
+                self.inventory.appears()
+
+            self.slide_scene = 0.
+            return
+
+        self.in_menu = False
+
+        #sliding
         steps = range(SLIDE_SPEED,0,-1)
 
         if x <= SIDE_SCREEN:
@@ -62,6 +89,8 @@ class MultiScene(object):
             self.slide_scene = 1 * steps[int((SCREEN_SIZE[0] - x)/float(SIDE_SCREEN) * (SLIDE_SPEED-1))]
         else:
             self.slide_scene = 0.
+
+
 
     def slide(self):
         self.position = self.position[0] + self.slide_scene, self.position[1]
@@ -95,26 +124,43 @@ class GameScene(cocos.scene.Scene):
                     (SCREEN_SIZE[0] - SIDE_SCREEN,SCREEN_SIZE[1])
                     ],
                     [
-                    (0, TOP_SIDE[1]),
-                    (SCREEN_SIZE[0], TOP_SIDE[1])
+                    (0, TOP_SIDE),
+                    (SCREEN_SIZE[0], TOP_SIDE)
                     ]
                 ]
         for line in lines:
             line = cocos.draw.Line(line[0],line[1],color)
-
-        self.add(linev1,z=10)
-        self.add(linev2,z=10)
+            self.add(line,z=10)
 
         #add
         self.add(self.layer,z=0)
         self.add(self.filter_layer,z=1)
 
-class Inventory(cocos.layer.Layer):
+class Inventory(cocos.sprite.Sprite):
 
     def __init__(self):
-        cocos.layer.Layer.__init__(self)
+        
+        image = pyglet.image.load('img/gui/menu.png')
+        anchor = image.width/2,0
 
-    def appears(self,visible = True):
-        pass
+        position = SCREEN_SIZE[0]/2,-1 * image.height # position below the screen
+        cocos.sprite.Sprite.__init__(self,image,position=position,anchor=anchor)
+
+        self.appearance_time = APPEARANCE_TIME
+
+    def appears(self,appears=True):
+        if len(self.actions) > 0:
+            return
+        
+        sens = 1
+        if not appears:
+            sens = -1
+
+        delta = 0, self.image.height * sens
+        move = cocos.actions.interval_actions.MoveBy(delta,duration = 0.5)
+        self.do(move)
+
+
+        
 
 
